a8eacec209b8c8b7d5c42362a7c11a2c7080829c
thanoskat <thanoskat@gmx.com>
2025-02-23 01:15:46 +0200

    feat: join - make join commands case-insensitive

--- SplitServer/Commands/RevokeJoinTokenCommandHandler.cs
+++ SplitServer/Commands/RevokeJoinTokenCommandHandler.cs
@@ -5,13 +5,13 @@ using SplitServer.Repositories;
 
 namespace SplitServer.Commands;
 
-public class UseJoinTokenCommandHandler : IRequestHandler<UseJoinTokenCommand, Result>
+public class RevokeJoinTokenCommandHandler : IRequestHandler<RevokeJoinTokenCommand, Result>
 {
     private readonly IUsersRepository _usersRepository;
     private readonly IJoinTokensRepository _joinTokensRepository;
     private readonly IGroupsRepository _groupsRepository;
 
-    public UseJoinTokenCommandHandler(
+    public RevokeJoinTokenCommandHandler(
         IUsersRepository usersRepository,
         IJoinTokensRepository joinTokensRepository,
         IGroupsRepository groupsRepository)
@@ -21,7 +21,7 @@ public class UseJoinTokenCommandHandler : IRequestHandler<UseJoinTokenCommand, R
         _groupsRepository = groupsRepository;
     }
 
-    public async Task<Result> Handle(UseJoinTokenCommand command, CancellationToken ct)
+    public async Task<Result> Handle(RevokeJoinTokenCommand command, CancellationToken ct)
     {
         var userMaybe = await _usersRepository.GetById(command.UserId, ct);
 
@@ -41,16 +41,6 @@ public class UseJoinTokenCommandHandler : IRequestHandler<UseJoinTokenCommand, R
 
         var joinToken = joinTokenMaybe.Value;
 
-        if (joinToken.Expires < DateTime.UtcNow)
-        {
-            return Result.Failure($"Join token {command.JoinToken} is expired");
-        }
-
-        if (joinToken.TimesUsed >= joinToken.MaxUses)
-        {
-            return Result.Failure($"Join token {command.JoinToken} has reached maximum number of uses");
-        }
-
         var groupMaybe = await _groupsRepository.GetById(joinToken.GroupId, ct);
 
         if (groupMaybe.HasNoValue)
@@ -65,35 +55,7 @@ public class UseJoinTokenCommandHandler : IRequestHandler<UseJoinTokenCommand, R
             return Result.Failure("You are already a group member");
         }
 
-        var now = DateTime.UtcNow;
-
-        var newMember = new Member
-        {
-            Id = Guid.NewGuid().ToString(),
-            UserId = command.UserId,
-            Joined = now
-        };
-
-        var updatedGroup = group with
-        {
-            Members = group.Members.Concat([newMember]).ToList(),
-            Updated = now
-        };
-
-        var updateGroupResult = await _groupsRepository.Update(updatedGroup, ct);
-
-        if (updateGroupResult.IsFailure)
-        {
-            return updateGroupResult;
-        }
-
-        var updatedJoinToken = joinToken with
-        {
-            TimesUsed = joinToken.TimesUsed + 1,
-            Updated = now
-        };
-
-        var updateJoinTokenResult = await _joinTokensRepository.Update(updatedJoinToken, ct);
+        var updateJoinTokenResult = await _joinTokensRepository.Delete(normalizedJoinToken, ct);
 
         if (updateJoinTokenResult.IsFailure)
         {
