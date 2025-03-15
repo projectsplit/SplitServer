4f513b923cc3f374e9cb7a5bb042be0cba7c3db0
ChristosKehayas <christos.kechagia@gmail.com>
2025-03-15 19:07:46 +0000

    added a ? to GooglePlace Id string

--- SplitServer/Models/GooglePlace.cs
+++ SplitServer/Models/GooglePlace.cs
@@ -2,7 +2,7 @@
 
 public class GooglePlace
 {
-    public required string Id { get; init; }
+    public required string? Id { get; init; }
     public required string Name { get; init; }
     public required string Address { get; init; }
 }
