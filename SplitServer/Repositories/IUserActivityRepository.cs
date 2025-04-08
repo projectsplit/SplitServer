dcd2e7a70f249ad18e875f7d0fbf63978a77b812
thanoskat <thanoskat@gmx.com>
2025-04-08 23:42:47 +0300

    fix: set updated field in update repo methods

--- SplitServer/Repositories/IUserActivityRepository.cs
+++ SplitServer/Repositories/IUserActivityRepository.cs
@@ -5,7 +5,7 @@ namespace SplitServer.Repositories;
 
 public interface IUserActivityRepository : IRepositoryBase<UserActivity>
 {
-    Task<Result> ClearRecentGroupForUser(string userId, string groupId, CancellationToken ct);
+    Task<Result> ClearRecentGroupForUser(string userId, string groupId, DateTime updated, CancellationToken ct);
 
-    Task<Result> ClearRecentGroupForAllUsers(string groupId, CancellationToken ct);
+    Task<Result> ClearRecentGroupForAllUsers(string groupId, DateTime updated, CancellationToken ct);
 }
