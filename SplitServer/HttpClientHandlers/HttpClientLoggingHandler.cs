2b474ca81553c36eebc955d2336c86914d7bc5f4
katiras <thanoskat@gmx.com>
2025-04-18 15:24:43 +0300

    fix: httpclient logging - make log context local

--- SplitServer/HttpClientHandlers/HttpClientLoggingHandler.cs
+++ SplitServer/HttpClientHandlers/HttpClientLoggingHandler.cs
@@ -1,34 +1,28 @@
 ﻿using Serilog;
+using Serilog.Context;
 
 namespace SplitServer.HttpClientHandlers;
 
 public class HttpClientLoggingHandler : DelegatingHandler
 {
-    private readonly IDiagnosticContext _diagnosticContext;
-
-    public HttpClientLoggingHandler(IDiagnosticContext diagnosticContext)
-    {
-        _diagnosticContext = diagnosticContext;
-    }
-
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken ct)
     {
         var response = await base.SendAsync(request, ct);
 
-        _diagnosticContext.Set("RequestUri", request.RequestUri?.ToString() ?? string.Empty);
+        using (LogContext.PushProperty("RequestUri", request.RequestUri?.ToString() ?? string.Empty));
 
         var requestHeaders = string.Join(" \n", request.Headers.Select(h => $"{h.Key}: {h.Value}"));
-        _diagnosticContext.Set("RequestHeaders", requestHeaders);
+        using (LogContext.PushProperty("RequestHeaders", requestHeaders));
 
         if (request.Content is not null)
         {
-            _diagnosticContext.Set("RequestBody", await request.Content.ReadAsStringAsync(ct));
+            using (LogContext.PushProperty("RequestBody", await request.Content.ReadAsStringAsync(ct)));
         }
 
         var responseHeaders = string.Join(" \n", response.Headers.Select(h => $"{h.Key}: {h.Value}"));
-        _diagnosticContext.Set("ResponseHeaders", responseHeaders);
+        using (LogContext.PushProperty("ResponseHeaders", responseHeaders));
 
-        _diagnosticContext.Set("ResponseBody", await response.Content.ReadAsStringAsync(ct));
+        using (LogContext.PushProperty("ResponseBody", await response.Content.ReadAsStringAsync(ct)));
 
         Log.Information(
             "HTTP CLIENT: {Method} {Uri} {StatusCode} {ReasonPhrase}",
