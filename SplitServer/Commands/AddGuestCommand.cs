3de97d77cd467bf8847e4bd39fa47495bb69f635
ChristosKehayas <christos.kechagia@gmail.com>
2025-06-07 12:17:40 +0100

    return guest

--- SplitServer/Commands/AddGuestCommand.cs
+++ SplitServer/Commands/AddGuestCommand.cs
@@ -6,6 +6,7 @@ namespace SplitServer.Commands;
 
 public class AddGuestCommand : IRequest<Result<Guest>>
 {
+    
     public required string UserId { get; init; }
     public required string GroupId { get; init; }
     public required string GuestName { get; init; }
