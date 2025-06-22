6d53e2464ca82b9bd28be3510d09429c4abd6956
katiras <thanoskat@gmx.com>
2025-06-22 14:24:42 +0300

    feat: search expenses date

--- SplitServer/Models/Label.cs
+++ SplitServer/Models/Label.cs
@@ -5,4 +5,6 @@ public record Label
     public required string Id { get; init; }
     public required string Text { get; init; }
     public required string Color { get; init; }
+
+    public static readonly Label Empty = new() { Id = "", Text = "", Color = "" };
 }
