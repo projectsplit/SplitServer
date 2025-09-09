5af92de152d7f638f05d6d119d04a2e86e117439
katiras <thanoskat@gmx.com>
2025-09-09 19:03:32 +0300

    feat: add join code usernames in GetGroupJoinCodesQueryHandler

--- SplitServer/Responses/GetGroupJoinCodesResponse.cs
+++ SplitServer/Responses/GetGroupJoinCodesResponse.cs
@@ -1,9 +1,7 @@
-﻿using SplitServer.Models;
-
-namespace SplitServer.Responses;
+﻿namespace SplitServer.Responses;
 
 public class GetGroupJoinCodesResponse
 {
-    public required List<JoinCode> Codes { get; init; }
+    public required List<JoinCodeResponseItem> Codes { get; init; }
     public required string? Next { get; init; }
 }
---
+++ SplitServer/Responses/JoinCodeResponseItem.cs
@@ -0,0 +1,14 @@
+﻿namespace SplitServer.Responses;
+
+public record JoinCodeResponseItem
+{
+    public required string Id { get; init; }
+    public required DateTime Created { get; init; }
+    public required DateTime Updated { get; init; }
+    public required string GroupId { get; init; }
+    public required string CreatorId { get; init; }
+    public required string CreatorUsername { get; init; }
+    public required int TimesUsed { get; init; }
+    public required int MaxUses { get; init; }
+    public required DateTime Expires { get; init; }
+}
