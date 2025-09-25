8f820ce4d58a5356ff964e0745bb119977320135
katiras <thanoskat@gmx.com>
2025-09-25 23:33:05 +0300

    feat: log user id when logging out

--- SplitServer/Commands/LogOutCommandHandler.cs
+++ SplitServer/Commands/LogOutCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using CSharpFunctionalExtensions;
 using MediatR;
+using Serilog;
 using SplitServer.Repositories;
 using SplitServer.Services.Auth;
 
@@ -8,16 +9,26 @@ namespace SplitServer.Commands;
 public class LogOutCommandHandler : IRequestHandler<LogOutCommand, Result>
 {
     private readonly ISessionsRepository _sessionsRepository;
+    private readonly IDiagnosticContext _diagnosticContext;
 
     public LogOutCommandHandler(
         ISessionsRepository sessionsRepository,
-        AuthService authService)
+        AuthService authService,
+        IDiagnosticContext diagnosticContext)
     {
         _sessionsRepository = sessionsRepository;
+        _diagnosticContext = diagnosticContext;
     }
 
     public async Task<Result> Handle(LogOutCommand command, CancellationToken ct)
     {
+        var sessionMaybe = await _sessionsRepository.GetByRefreshToken(command.RefreshToken, ct);
+
+        if (sessionMaybe.HasValue)
+        {
+            _diagnosticContext.Set("UserId", sessionMaybe.Value.UserId);
+        }
+
         return await _sessionsRepository.DeleteByRefreshToken(command.RefreshToken, ct);
     }
 }
