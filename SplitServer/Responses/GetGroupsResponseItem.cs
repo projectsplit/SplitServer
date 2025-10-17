6b3b2502b43edebd304e88bd9bfebe760099d301
ChristosKehayas <christos.kechagia@gmail.com>
2025-10-17 16:36:20 +0100

    added additional group props

--- SplitServer/Responses/GetGroupsResponseItem.cs
+++ SplitServer/Responses/GetGroupsResponseItem.cs
@@ -1,7 +1,17 @@
-﻿namespace SplitServer.Responses;
+﻿using SplitServer.Models;
+
+namespace SplitServer.Responses;
 
 public class GetGroupsResponseItem
 {
     public required string Id { get; init; }
     public required string Name { get; init; }
+    public required DateTime Created { get; init; }
+    public required DateTime Updated { get; init; }
+    public required string OwnerId { get; init; }
+    public required string Currency { get; init; }
+    public required bool IsArchived { get; init; }
+    public required List<Member> Members { get; init; }
+    public required List<Guest> Guests { get; init; }
+    public required List<Label> Labels { get; init; }
 }
