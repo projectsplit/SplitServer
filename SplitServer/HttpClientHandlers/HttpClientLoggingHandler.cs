baeb695681551bef2dd5249c9e8b595b5f8a966e
katiras <thanoskat@gmx.com>
2025-11-03 23:27:01 +0200

    chore: elapsed time in http client logs

--- SplitServer/HttpClientHandlers/HttpClientLoggingHandler.cs
+++ SplitServer/HttpClientHandlers/HttpClientLoggingHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
 using Serilog;
 using Serilog.Core;
 using Serilog.Core.Enrichers;
@@ -9,15 +10,21 @@ public class HttpClientLoggingHandler : DelegatingHandler
 {
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken ct)
     {
+        var stopwatch = Stopwatch.StartNew();
+
         var response = await base.SendAsync(request, ct);
+
+        stopwatch.Stop();
 
         if (!response.IsSuccessStatusCode)
         {
+            var requestBody = request.Content is not null ? await request.Content.ReadAsStringAsync(ct) : string.Empty;
+
             var logEventEnrichers = new ILogEventEnricher[]
             {
                 new PropertyEnricher("RequestUri", request.RequestUri?.ToString() ?? string.Empty),
                 new PropertyEnricher("RequestHeaders", HeaderString(request.Headers)),
-                new PropertyEnricher("RequestBody", request.Content is not null ? await request.Content.ReadAsStringAsync(ct) : string.Empty),
+                new PropertyEnricher("RequestBody", requestBody),
                 new PropertyEnricher("ResponseHeaders", HeaderString(response.Headers)),
                 new PropertyEnricher("ResponseBody", await response.Content.ReadAsStringAsync(ct)),
             };
@@ -25,31 +32,29 @@ public class HttpClientLoggingHandler : DelegatingHandler
             Log
                 .ForContext(logEventEnrichers)
                 .Error(
-                    "HTTP CLIENT {Method} {Uri} {StatusCode} {ReasonPhrase}",
+                    "HTTP CLIENT {Method} {Uri} {StatusCode} {ReasonPhrase} in {} ms",
                     request.Method,
                     request.RequestUri,
                     (int)response.StatusCode,
-                    response.ReasonPhrase);
+                    response.ReasonPhrase,
+                    stopwatch.ElapsedMilliseconds);
         }
         else
         {
-            Log.Information(
-                "HTTP CLIENT {Method} {Uri} {StatusCode} {ReasonPhrase}",
-                request.Method,
-                request.RequestUri,
-                (int)response.StatusCode,
-                response.ReasonPhrase);
+            Log
+                .Information(
+                    "HTTP CLIENT {Method} {Uri} {StatusCode} {ReasonPhrase} in {} ms",
+                    request.Method,
+                    request.RequestUri,
+                    (int)response.StatusCode,
+                    response.ReasonPhrase,
+                    stopwatch.ElapsedMilliseconds);
         }
 
         return response;
     }
 
-    private static string HeaderString(HttpRequestHeaders header)
-    {
-        return string.Join("\n", header.Select(h => $"{h.Key}: {string.Join(" ", h.Value)}"));
-    }
-
-    private static string HeaderString(HttpResponseHeaders header)
+    private static string HeaderString(HttpHeaders header)
     {
         return string.Join("\n", header.Select(h => $"{h.Key}: {string.Join(" ", h.Value)}"));
     }
