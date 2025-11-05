7c7bb8d3b866f09674f9c76a292bf9d9446dd057
ChristosKehayas <christos.kechagia@gmail.com>
2025-11-05 21:57:35 +0000

    group search

---
+++ SplitServer/Queries/SearchByGroupNameQuery.cs
@@ -0,0 +1,13 @@
+﻿using CSharpFunctionalExtensions;
+using MediatR;
+using SplitServer.Responses;
+
+namespace SplitServer.Queries;
+
+public class SearchByGroupNameQuery : IRequest<Result<GetGroupsResponse>>
+{
+    public required string UserId { get; init; }
+    public required int PageSize { get; init; }
+    public required string? Keyword { get; init; }
+    public required string? Next { get; init; }
+}
